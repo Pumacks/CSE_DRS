d497e8e0a018008f702270fafbc6e2b7cb8d6250
Roalb Tashkollari <roalbtashkollari@gmail.com>
2024-10-22 23:40:21 +0200

    init animation

---
+++ Models/Entities/Animation.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStateManagementSample.Models.Entities
+{
+    public class Animation
+    {
+
+        public static float TotalSeconds { get; set; }
+        public static ContentManager Content { get; set; }
+        public static SpriteBatch SpriteBatch { get; set; }
+
+        public static void Update(GameTime gt)
+        {
+            TotalSeconds = (float)gt.ElapsedGameTime.TotalSeconds;
+        }
+
+
+        private readonly Texture2D _texture;
+        private readonly List<Rectangle> _sourceRectangles = [];
+        private readonly int _frames;
+        private int _frame;
+        private readonly float _frameTime;
+        private float _frameTimeLeft;
+        private bool _active = true;
+
+        public Animation(Texture2D texture, int framesX, int framesY, float frameTime, int row = 1)
+        {
+            _texture = texture;
+            _frameTime = frameTime;
+            _frameTimeLeft = _frameTime;
+            _frames = framesX;
+            var frameWidth = _texture.Width / framesX;
+            var frameHeight = _texture.Height / framesY;
+
+            for (int i = 0; i < _frames; i++)
+            {
+                _sourceRectangles.Add(new(i * frameWidth, (row - 1) * frameHeight, frameWidth, frameHeight));
+            }
+        }
+
+        public void Stop()
+        {
+            _active = false;
+        }
+
+        public void Start()
+        {
+            _active = true;
+        }
+
+        public void Reset()
+        {
+            _frame = 0;
+            _frameTimeLeft = _frameTime;
+        }
+
+        public void Update()
+        {
+            if (!_active) return;
+
+            _frameTimeLeft -= TotalSeconds;
+
+            if (_frameTimeLeft <= 0)
+            {
+                _frameTimeLeft += _frameTime;
+                _frame = (_frame + 1) % _frames;
+            }
+        }
+
+        public void Draw(Vector2 pos)
+        {
+            SpriteBatch.Draw(_texture, pos, _sourceRectangles[_frame], Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 1);
+        }
+    }
+}
