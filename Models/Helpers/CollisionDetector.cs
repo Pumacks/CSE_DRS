4c7cdba606f59aca7f7820ff615ef954494f4feb
Roalb Tashkollari <roalbtashkollari@gmail.com>
2024-11-09 22:14:24 +0100

    CollisionDetector class

---
+++ Models/Helpers/CollisionDetector.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStateManagementSample.Models.Helpers
+{
+    public class CollisionDetector
+    {
+        public static bool IsIntersecting(Rectangle objA, Rectangle objB)
+        {
+            return objA.Intersects(objB);
+        }
+    }
+}
