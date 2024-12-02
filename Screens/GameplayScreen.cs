5fb252780329b93a94d5fdcf2ae7a8ed3f90e6ef
Daniel Beck <daniel.beck99@outlook.de>
2024-12-02 18:50:57 +0100

    hero spawns better

--- Screens/GameplayScreen.cs
+++ Screens/GameplayScreen.cs
@@ -102,7 +102,7 @@ namespace GameStateManagement
 
 
 
-            hero = new Player(100, 5, new Vector2(5000, 5000), golem, gameFont, new List<Item>());
+            hero = new Player(100, 5, new Vector2(5400, 5400), golem, gameFont, new List<Item>());
 
 
             map.LoadMapTextures(content);
