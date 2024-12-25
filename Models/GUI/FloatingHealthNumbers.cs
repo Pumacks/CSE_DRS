a9a26693a7fe8bcd8304a043c1169d95a4b2cf48
Roalb Tashkollari <roalbtashkollari@gmail.com>
2024-12-25 14:31:42 +0100

    simple combat damage indicator

--- Models/GUI/FloatingHealthNumbers.cs
+++ Models/GUI/FloatingHealthNumbers.cs
@@ -6,19 +6,27 @@ using Microsoft.Xna.Framework;
 
 namespace GameStateManagementSample.Models.GUI
 {
+    enum Health
+    {
+        Damage,
+        Healing
+    }
     internal class FloatingHealthNumbers : GUIObserver
     {
         private Color color;
         private int healthPoints;
         private int healthDifference;
-        private double maxTime = 200;
         bool healthChanged = false;
+
+        private Health healthStatus;
+
+        private double maxTime = 200;
         private Vector2 offset;
 
         public FloatingHealthNumbers(Entity entity) : base(entity)
         {
             healthPoints = entity.HealthPoints;
-            offset = new Vector2(0, entity.Texture.Width / 4);
+            offset = new Vector2(-30, (entity.Texture.Width / 4) -5);
         }
 
         public override void Update()
@@ -32,16 +40,23 @@ namespace GameStateManagementSample.Models.GUI
                 maxTime = 2000;
                 if (oldHealthPoints < healthPoints)
                 {
+                    if (healthStatus == Health.Damage)
+                        healthDifference = 0;
+
+                    healthStatus = Health.Healing;
                     color = Color.Green;
-                    healthDifference = healthPoints - oldHealthPoints;
+                    healthDifference += healthPoints - oldHealthPoints;
                 }
                 else
                 {
+                    if (healthStatus == Health.Healing)
+                        healthDifference = 0;
+
+                    healthStatus = Health.Damage;
                     color = Color.Red;
-                    healthDifference = oldHealthPoints - healthPoints;
+                    healthDifference -= oldHealthPoints - healthPoints;
                 }
             }
-
         }
         public override void Draw(SpriteBatch spriteBatch, SpriteFont spriteFont)
         {
@@ -52,9 +67,10 @@ namespace GameStateManagementSample.Models.GUI
                 {
                     healthChanged = false;
                     maxTime = 2000;
+                    healthDifference = 0;
                 }
 
-                String str = color == Color.Green ? "+" : "-";
+                String str = healthDifference > 0 ? "+" : "";
                 spriteBatch.Begin(transformMatrix: player.CameraProperty.Transform);
                 spriteBatch.DrawString(spriteFont, str + healthDifference.ToString(), player.Position - spriteFont.MeasureString(healthDifference.ToString()) - offset, color);
 
